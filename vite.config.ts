731bff19202621462a1afa3c899d48d1097bde09
desperati0n <2289909644@qq.com>
2025-11-03 22:55:25 +0800

    set-custom-domain-ismism.example

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // 如果仓库名为 ismism（项目页），请保留 base；若为用户页仓库，可改为 '/'。
-  base: '/ismism/',
+  // 自定义域名直达根路径，base 必须为 '/'
+  base: '/',
   plugins: [react()],
 })
 
