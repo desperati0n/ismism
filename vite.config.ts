82a7a39faa6406c652eede7447c15eebeb19e78e
desperati0n <2289909644@qq.com>
2025-12-26 22:10:37 +0800

    fix: 修复 GitHub Pages 部署路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // 自定义域名直达根路径，base 必须为 '/'
-  base: '/',
+  // GitHub Pages 部署路径
+  base: '/ismism/',
   plugins: [react()],
 })
 
